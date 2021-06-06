cd1fca042855ddb27e9c7acff32e93deef202478
Alin Cristian <alin.cristian977@gmail.com>
2021-06-06 19:59:00 +0200

    Added Splash and Start screens
    Imported Splash and Start screens images and audio
    Created a Level Loader object

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] private float _timeToWaitInSeconds;
+    private int _currentSceneIndex;
+
+    private void Start()
+    {
+        _currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (_currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitForTime());
+        }
+    }
+
+    private IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(_timeToWaitInSeconds);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(_currentSceneIndex + 1);
+    }
+}

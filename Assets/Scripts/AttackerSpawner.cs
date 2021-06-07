1d8128926e924e2d938be2e207c4c2b550b3ebec
Alin Cristian <alin.cristian977@gmail.com>
2021-06-07 22:25:05 +0200

    Added Attacker Spawners on each Lane

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    private bool _spawn = true;
+    [SerializeField] private float _minSpawnDelay;
+    [SerializeField] private float _maxSpawnDelay;
+    [SerializeField] private Attacker _attacker;
+
+
+    private IEnumerator Start()
+    {
+        while (_spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(_minSpawnDelay, _maxSpawnDelay));
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(_attacker, transform.position, transform.rotation);
+    }
+}

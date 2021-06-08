bdf10f1fcc7e00403d7dfd007f74a2543a236f22
Alin Cristian <alin.cristian977@gmail.com>
2021-06-08 13:18:30 +0200

    Added Health for Lizard and Damage for Projectile
    Added colliders and rigidbodies for Lizard and Projectile prefabs
    Lizard receives Damage on Collision with the Projectile

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,10 +6,24 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] private float _speed = 1f;
+    [SerializeField] private float _damage = 50f;
 
 
     private void Update()
     {
         transform.Translate(Vector2.right * (_speed * Time.deltaTime));
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var healthComponent = other.GetComponent<Health>();
+        
+        Debug.Log("collision");
+        healthComponent.DealDamage(_damage);
+    }
+
+    private void DealDamage(float damage)
+    {
+        
     }
 }

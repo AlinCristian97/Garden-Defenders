392240b1a78d1de24e4e4d2553b8c8ed752d153f
Alin Cristian <alin.cristian977@gmail.com>
2021-06-08 18:32:11 +0200

    fix: Corrected the Defenders Spawn position so that it snaps on the Grid

--- Assets/Scripts/DefenderSpawner.cs
+++ Assets/Scripts/DefenderSpawner.cs
@@ -11,11 +11,31 @@ public class DefenderSpawner : MonoBehaviour
     private void OnMouseDown()
     {
         Debug.Log("Mouse clicked");
-        SpawnDefender();
+        SpawnDefender(GetSquareClicked());
     }
 
-    private void SpawnDefender()
+    private Vector2 GetSquareClicked()
     {
-        GameObject newDefender = Instantiate(_defender, transform.position, Quaternion.identity);
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+
+        Vector2 gridPos = SnapToGrid(worldPos);
+
+        return gridPos;
+    }
+
+    private Vector2 SnapToGrid(Vector2 rawWorldPos)
+    {
+        float newX = Mathf.RoundToInt(rawWorldPos.x);
+        float newY = Mathf.RoundToInt(rawWorldPos.y);
+
+        return new Vector2(newX, newY);
+    }
+
+    private void SpawnDefender(Vector2 roundedPos)
+    {
+        GameObject newDefender = Instantiate(_defender, roundedPos, Quaternion.identity);
+        Debug.Log(roundedPos);
     }
 }

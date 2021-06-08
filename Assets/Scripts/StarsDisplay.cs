22656f91e627fefe065529e887d0f0e33cecdbef
Alin Cristian <alin.cristian977@gmail.com>
2021-06-08 19:35:42 +0200

    Started implementing the Resource System
    Created resource variable
    Displayed resource on UI

---
+++ Assets/Scripts/StarsDisplay.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarsDisplay : MonoBehaviour
+{
+    [SerializeField] private int _stars = 100;
+    private Text _starsText;
+
+    private void Awake()
+    {
+        _starsText = GetComponent<Text>();
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        _starsText.text = _stars.ToString();
+    }
+
+    public void AddStars(int amount)
+    {
+        _stars += amount;
+        UpdateDisplay();
+    }
+
+    public void SpendStars(int amount)
+    {
+        if (_stars >= amount)
+        {
+            _stars -= amount;
+            UpdateDisplay(); 
+        }
+    }
+}

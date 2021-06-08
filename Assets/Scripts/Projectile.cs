c72ccb56e9d91d17a94c91efdf6f67e4b1712964
Alin Cristian <alin.cristian977@gmail.com>
2021-06-08 11:56:24 +0200

    Added Move behaviour to the Projectile

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float _speed = 1f;
+
+
+    private void Update()
+    {
+        transform.Translate(Vector2.right * (_speed * Time.deltaTime));
+    }
+}

eb94792977837922884c9149ac5fe685d7760e4f
Alin Cristian <alin.cristian977@gmail.com>
2021-06-09 22:02:28 +0200

    Created new Attacker (Fox)
    Implemented Fox special ability (Jump over Gravestone)
    Restructured 'Assets' folder

---
+++ Assets/Scripts/Defender/Gravestone.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravestone : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var attacker = other.GetComponent<Attacker>();
+
+        if (attacker)
+        {
+            
+        }
+    }
+}

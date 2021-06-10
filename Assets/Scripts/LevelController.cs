b43d57f77ffc71e7fd94f795d899625dec350a48
Alin Cristian <alin.cristian977@gmail.com>
2021-06-10 14:22:32 +0200

    enhancement: Improved User Experience upon winning the level
        Display winner message on UI
        Play winner sound
        Load next level
        Added 'Level 2' scene (duplicated 'Level 1')

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -1,11 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelController : MonoBehaviour
 {
+    [SerializeField] private GameObject _winLabel;
+    [SerializeField] private float _waitToLoad;
+
     private int _numberOfAttackers = 0;
     private bool _levelTimerFinished = false;
+
+    private void Start()
+    {
+        _winLabel.SetActive(false);
+    }
 
     public void AttackerSpawned()
     {
@@ -18,7 +27,17 @@ public class LevelController : MonoBehaviour
         if (_numberOfAttackers <= 0 && _levelTimerFinished)
         {
             Debug.Log("End level now!");
+            StartCoroutine(HandleWinCondition());
         }
+    }
+
+    private IEnumerator HandleWinCondition()
+    {
+        _winLabel.SetActive(true);
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(_waitToLoad);
+        
+        FindObjectOfType<LevelLoader>().LoadNextScene();
     }
 
     public void LevelTimerFinished()

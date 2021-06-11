b0d4a6651fb219515e27d20440a5afc33a692c07
Alin Cristian <alin.cristian977@gmail.com>
2021-06-11 19:43:28 +0200

    Added Defender Star Cost text on UI

--- Assets/Scripts/DefenderButton.cs
+++ Assets/Scripts/DefenderButton.cs
@@ -2,11 +2,31 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DefenderButton : MonoBehaviour
 {
     [SerializeField] private Defender _defenderPrefab;
-    
+
+    private void Start()
+    {
+        LabelButtonWithCost();
+    }
+
+    private void LabelButtonWithCost()
+    {
+        Text costText = GetComponentInChildren<Text>();
+
+        if (!costText)
+        {
+            Debug.LogError(name + "has no cost text, add some!");
+        }
+        else
+        {
+            costText.text = _defenderPrefab.GetStarCost().ToString();
+        }
+    }
+
     private void OnMouseDown()
     {
         var buttons = FindObjectsOfType<DefenderButton>();

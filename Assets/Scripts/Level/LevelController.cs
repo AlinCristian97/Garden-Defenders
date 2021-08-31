a6dacb9ce924a23af7e1163b783598d87e2c98dd
Alin Apostol <alin.cristian977@gmail.com>
2021-08-31 15:41:02 +0300

    Restructured Project Assets folder
    Deleted unused assets/scripts
    Moved certain assets/script

---
+++ Assets/Scripts/Level/LevelController.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    [SerializeField] private GameObject _winLabel;
+    [SerializeField] private GameObject _loseLabel;
+    [SerializeField] private float _waitToLoad;
+
+    private int _numberOfAttackers = 0;
+    private bool _levelTimerFinished = false;
+
+    private void Start()
+    {
+        _winLabel.SetActive(false);
+        _loseLabel.SetActive(false);
+    }
+
+    public void AttackerKilled()
+    {
+        _numberOfAttackers--;
+        if (_numberOfAttackers <= 0 && _levelTimerFinished)
+        {
+            Debug.Log("End level now!");
+            StartCoroutine(HandleWinCondition());
+        }
+    }
+
+    private IEnumerator HandleWinCondition()
+    {
+        _winLabel.SetActive(true);
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(_waitToLoad);
+        
+        FindObjectOfType<LevelLoader>().LoadNextScene();
+    }
+
+    public void LevelTimerFinished()
+    {
+        _levelTimerFinished = true;
+        StopSpawners();
+    }
+
+    private void StopSpawners()
+    {
+        
+    }
+    
+    public void HandleLoseCondition()
+    {
+        _loseLabel.SetActive(true);
+        Time.timeScale = 0;
+    }
+}

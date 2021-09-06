1c2506378363708247ba93269121abf1ffb934af
Alin Apostol <alin.cristian977@gmail.com>
2021-09-06 22:49:00 +0300

    Created AttackersArray & Enum to iterate through Level waves configs

--- Assets/Scripts/SpawnAttackers/AttackerSpawner.cs
+++ Assets/Scripts/SpawnAttackers/AttackerSpawner.cs
@@ -13,6 +13,18 @@ namespace SpawnAttackers
         [SerializeField] private float _timeBetweenSpawns = 10f;
         private float _nextSpawn;
 
-        
+        public void SpawnWave()
+        {
+            foreach (Attacker attacker in _spawnConfig.LevelWaves[0])
+            {
+                Debug.Log(name + " " + attacker.name);
+            }
+        }
+
+        private void Start()
+        {
+            Debug.Log("Test:");
+            SpawnWave();
+        }
     }
 }

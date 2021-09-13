fb03fd3dede32debebe1b5f3933edd5c796c8f13
Alin Apostol <alin.cristian977@gmail.com>
2021-09-13 06:05:57 +0300

    Added HealthHUD to all Defenders

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -1,11 +1,40 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using General.Patterns.FSM;
+using General.Patterns.Observer;
 using HealthSystem.Interfaces;
 using UnityEngine;
 
-public abstract class Unit : MonoBehaviour, IDamageable
+public abstract class Unit : MonoBehaviour, IDamageable, IObservable
 {
+    #region Observer
+
+    public List<IObserver> Observers { get; private set; } = new List<IObserver>();
+
+    public void AttachObserver(IObserver observer)
+    {
+        Observers.Add(observer);
+    }
+
+    public void DetachObserver(IObserver observer)
+    {
+        Observers.Remove(observer);
+    }
+
+    public void NotifyObservers()
+    {
+        if (Observers.Count > 0)
+        {
+            foreach (IObserver observer in Observers)
+            {
+                observer.GetNotified();
+            }
+        }
+    }
+
+    #endregion
+
     #region Components
 
     public Collider2D Collider { get; protected set; }
@@ -19,7 +48,19 @@ public abstract class Unit : MonoBehaviour, IDamageable
 
     #endregion
 
-    public int CurrentHealth { get; private set; }
+    private int _currentHealth;
+    public int CurrentHealth 
+    {
+        get
+        {
+            return _currentHealth;
+        }
+        private set
+        {
+            _currentHealth = value;
+            NotifyObservers();
+        } 
+    }
     [field: SerializeField] public int MaxHealth { get; private set; }
     
     public bool IsDead => CurrentHealth <= 0;
